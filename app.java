0cd18abf789d1027327835fc940e32aa39bebd38
Dominion Leo <leonelngenge@gmail.com>
2022-12-01 14:03:46 -0500

    application ready

--- app.java
+++ app.java
@@ -1,5 +1,5 @@
 # This is code for a web application 
 # Please do not hard code
 # make use of variables and dynamic variables
-# author: simon Leo
+# author: simon Leonel Ngenge
 # This is a front end application
